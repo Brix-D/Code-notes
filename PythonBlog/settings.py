518f96dbe3b15af6bd048ed0c59e200d4ba20117
Brix-D <Shlyakhta4@gmail.com>
2020-12-28 18:23:55 +0400

    Revert "Merge branch 'main' of https://github.com/Brix-D/Code-notes into main"
    This reverts commit 2927021ddd076b60699f27d8d63ca434b395cc5d, reversing
    changes made to 157c86dd26d49f7f8d1e8cc55dfee71b17acbca4.

--- PythonBlog/settings.py
+++ PythonBlog/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '7npgruwaro^ag8ryue#w#zx&l-pp6ze^0n)m&8e^rq#_@=ye#u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
