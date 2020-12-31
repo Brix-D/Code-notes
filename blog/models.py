57572cbcc547d932f87ac94d3142c8652d3043c3
Brix-D <Shlyakhta4@gmail.com>
2020-12-31 16:42:13 +0400

    Added handling photo files, when article is updated, garbage photos have to delete

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-from django.db.models.signals import post_delete
+# from django.db.models import ObjectDoesNotExist
+from django.db.models.signals import post_delete, pre_save
 from django.shortcuts import reverse
 from django.dispatch import receiver
+
+
 # Create your models here.
 
 
@@ -26,6 +29,19 @@ class Note(models.Model):
     def __str__(self):
         return self.title
 
+    # Еще один способ, запасной вариант
+    # def save(self, *args, **kwargs):
+    #     """Переобпределение метода сохранения модели
+    #      для обработки мусорных изображений,
+    #      оставшихся после обновления"""
+    #     try:
+    #         this = Note.objects.get(pk=self.id)
+    #         if this.picture != self.picture:
+    #             this.picture.delete(save=False)  # отмена автоматического вызова метода save, что вызовет рекурсию
+    #     except ObjectDoesNotExist:
+    #         print("запись не существует")
+    #     super(Note, self).save(*args, **kwargs)
+
 
 @receiver(post_delete, sender=Note)
 def submission_delete(sender, instance, **kwargs):
@@ -36,12 +52,34 @@ def submission_delete(sender, instance, **kwargs):
     instance.picture.delete(False)
 
 
+@receiver(pre_save, sender=Note)
+def delete_old_file_on_save(sender, instance, **kwargs):
+    """
+    Удаление старого фото при обновлении статьи
+    https://stackoverflow.com/questions/2878490/how-to-delete-old-image-when-update-imagefield
+    """
+    if instance.pk:  # При добавлении первичный ключ будет None
+        try:
+            old_picture = Note.objects.get(pk=instance.pk).picture  # Получение старого объекта фото
+        except Note.DoesNotExist:  # Если запись не существует
+            return
+        else:
+            new_picture = instance.picture  # Получение новго фото из объекта сигнала save - формы
+            if old_picture and old_picture != new_picture:
+                # Если старое фото существует и не равно новому фото,
+                # то удалить старое
+                old_picture.delete(save=False)
+                # Удаление старого фото, save=False отменяет вызов метода save для избежания рекурсии,
+                # сигнал save уже вызван когда управление перешло к этому обработчику
+
+
 class Comment(models.Model):
     """Комментарий к статье"""
     author = models.TextField(max_length=45, blank=False, verbose_name='Автор комментария')
     text = models.TextField(max_length=400, blank=False, verbose_name='Текст комментария')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     art = models.ForeignKey(Note, on_delete=models.CASCADE, verbose_name='Номер статьи')
+
     # Внешний ключ на Id статьи, при удалении статьи
     # комментарии удаляются каскадно
 
